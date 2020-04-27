e9ec27e24dcada443618a638ac62b6d60e1284c3
Sandeep Kumar Sharma <sandeepkumar.sharma@happay.in>
2020-04-27 16:48:59 +0530

    changed email id

--- ippa/Ippa_v1/Ippa_v1/settings.py
+++ ippa/Ippa_v1/Ippa_v1/settings.py
@@ -139,7 +139,8 @@ STATIC_URL = '/static/'
 
 #Email testing 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'help@ippa.in'
+EMAIL_HOST_USER = 'noreply@ippa.in'
 EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
+EMAIL_PORT = 587
